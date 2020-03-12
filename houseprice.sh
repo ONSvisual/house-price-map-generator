89df7bcb2556a7afe12b256fd5e5c0fe494ff670
hartlz <zoe.hartland@ons.gov.uk>
2020-03-12 14:49:15 +0000

    Create additional boundaries for zoomed out view

--- houseprice.sh
+++ houseprice.sh
@@ -70,6 +70,32 @@ echo 'making LSOA boundaries tiles'
 #makes tiles for the lsoa boundaries
 tippecanoe --minimum-zoom=10 --maximum-zoom=13 --output-to-directory boundaries --no-tile-size-limit boundaries.geojson
 
+echo 'downloading MSOA boundaries'
+#download generalised lsoa geojson
+wget https://opendata.arcgis.com/datasets/c661a8377e2647b0bae68c4911df868b_3.geojson
+
+echo 'rename file'
+#ogr2ogr didn't take kindly to a string beginning with a number
+mv c661a8377e2647b0bae68c4911df868b_3.geojson msoaboundaries2.geojson
+
+echo 'joining house prices data to MSOA boundaries'
+#drop fields we don't need
+ogr2ogr -f geojson -t_srs crs:84 -sql "SELECT msoa11cd, msoa11nm FROM msoaboundaries2" bounds.geojson msoaboundaries2.geojson
+
+#join house prices to boundaries too
+mapshaper-xl bounds.geojson -join houseprices.csv keys=msoa11cd,MSOAcode field-types=MSOAcode:str,	MSOAname:str,	LAcode:str,	LAname:str,	Regioncode:str,	Regionname:str,	income:num,	a_median:num,	d_median:num,	s_median:num,	t_median:num,	f_median:num,	a_lowerquartile:num,	d_lowerquartile:num,	s_lowerquartile:num,	t_lowerquartile:num,	f_lowerquartile:num,	a_tenp:num,	d_tenp:num,	s_tenp:num,	t_tenp:num,	f_tenp:num -o boundar.geojson
+
+#drop some more fields (probably way to many here at the moment) and convert to WGS84 projection
+ogr2ogr -f geojson -t_srs crs:84 -sql "SELECT msoa11cd, msoa11nm, LAcode,	LAname,	Regioncode,	Regionname,	income,	a_median,	d_median,	s_median,	t_median,	f_median,	a_lowerquartile,	d_lowerquartile,	s_lowerquartile,	t_lowerquartile,	f_lowerquartile,	a_tenp,	d_tenp,	s_tenp,	t_tenp,	f_tenp FROM boundar" boundaries.geojson boundar.geojson
+
+#tidy up
+rm bounds.geojson
+rm boundar.geojson
+
+echo 'making MSOA boundaries tiles'
+#makes tiles for the msoa boundaries
+tippecanoe --minimum-zoom=4 --maximum-zoom=9 --output-to-directory boundaries2 --no-tile-size-limit boundaries.geojson
+
 echo 'zipping up files, almost done'
 #zip the files up for EC2
 zip -r tiles.zip tiles boundaries
